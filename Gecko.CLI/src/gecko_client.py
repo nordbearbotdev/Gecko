108a00904d1a838c983dd74df1f39ed5bcb06a01
Commit 404 <85753549+nordbearbotdev@users.noreply.github.com>
2022-06-22 23:40:52 +0300

    Create gecko_client.py

---
+++ Gecko.CLI/src/gecko_client.py
@@ -0,0 +1,17 @@
+# импортируем всякое
+import SocksiPy.socks as socks
+import socket
+import threading
+import random
+import time
+import sys
+import os
+import shutil
+import subprocess
+import tempfile
+import hashlib
+import config
+import version
+
+TORCHAT_PORT = 11009 # не изменяй значение!!!
+TOR_CONFIG = "tor"
